1857f1ee676e0dd5b003f081fc2e857cdeafcb2d
silvamfpedro <silva.mfpedro@gmail.com>
2019-10-30 20:58:28 +0000

    Added changes in 3d-gl-object.py

--- augmented-reality-python/src/graphics/3d-gl-object.py
+++ augmented-reality-python/src/graphics/3d-gl-object.py
@@ -1,3 +1,66 @@
 # ------------------------
 #   IMPORTS
 # ------------------------
+# Import the necessary packages
+import pygame
+import OpenGL
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+# ------------------------
+#   MAIN FUNCTION
+# ------------------------
+def main():
+    pygame.init()
+    pygame.display.set_mode((600, 600), DOUBLEBUF|OPENGL)
+
+
+main()
+
+# Load the pygame parameters
+img = pygame.image.load('/home/pedro/Documents/augmented-reality/augmented-reality-python/assets/stones.jpg')
+textureData = pygame.image.tostring(img, "RGB", 1)
+width = img.get_width()
+height = img.get_height()
+
+# Load OpenGL parameters
+im = glGenTextures(1)
+glBindTexture(GL_TEXTURE_2D, im)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData)
+glEnable(GL_TEXTURE_2D)
+
+
+# ------------------------
+#   WALL FUNCTION
+# ------------------------
+def wall(image):
+    # set OpenGL parameters for the wall
+    glBegin(GL_QUADS)
+    glTexCoord2f(0, 0)
+    glVertex3f(-4, -4, -16)
+    glTexCoord2f(0, 1)
+    glVertex3f(-4, 4, -16)
+    glTexCoord2f(1, 1)
+    glVertex3f(4, 4, -8)
+    glTexCoord2f(1, 0)
+    glVertex3f(4, -4, -8)
+    glEnd()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    glLoadIdentity()
+    gluPerspective(45, 1, 0.05, 50)
+    glTranslatef(0, 0, -5)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    # set OpenGL wall parameters
+    wall(im)
+    # start pygame
+    pygame.display.flip()
+    pygame.time.wait(50)
+
